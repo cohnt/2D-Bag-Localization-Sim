aede1849011b5a5f55733d1f47d62e832e50ae03
Thomas Cohn <cohnt@umich.edu>
2018-08-13 10:02:27 -0400

    PARTICLE: Add draw method, as well as the other required methods and functions.

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ var canvasSize = {width: 800, height: 500};
 var bagHandleLocations = [[380, 280], [420, 220]];
 var bagHandleLength = 40;
 var bagHandleAngle = Math.PI / 6;
+var numParticles = 500;
+var particleDispRadius = 3;
+var errorColorDivisor = 10; //Error is mapped to color with e^(-x/k). This is constant is k.
 
 ///////////////////////////////////////////
 /// GLOBAL VARIABLES
@@ -13,19 +16,34 @@ var bagHandleAngle = Math.PI / 6;
 
 var canvas;
 var ctx;
+var particles = [];
 
 ///////////////////////////////////////////
 /// CLASSES
 ///////////////////////////////////////////
 
-function Particle(loc) {
-	this.loc = loc.slice();
+function Particle(pos) {
+	this.pos = pos.slice();
 	this.weight = 0;
 	this.isExploration = false;
 
 	this.randomize = function() {
 		this.pos = [Math.random() * canvasSize.width, Math.random() * canvasSize.height];
 		this.isExploration = true;
+	}
+	this.draw = function(ctx, maxWeight, mult) {
+		color = errorToColor(this.getError());
+		ctx.strokeStyle = color;
+		ctx.fillStyle = color;
+		ctx.beginPath();
+		ctx.moveTo(this.pos[0], this.pos[1]);
+		ctx.arc(this.pos[0], this.pos[1], particleDispRadius, 0, 2*Math.PI, true);
+		ctx.closePath();
+		ctx.fill();
+	}
+	this.getError = function() {
+		//TODO
+		return 0;
 	}
 }
 
@@ -74,6 +92,48 @@ function drawBag() {
 	ctx.stroke();
 
 	ctx.lineWidth = oldLineWidth;
+}
+function errorToColor(error) {
+	var mappedVal = Math.pow(Math.E, -1 * error / errorColorDivisor);
+
+	//Create HSL
+	var h = ((1-mappedVal)*240)/360;
+	var s = 1;
+	var l = 0.5;
+
+	//Convert to RGB (from https://gist.github.com/mjackson/5311256)
+	var r, g, b;
+
+	function hue2rgb(p, q, t) {
+		if (t < 0) t += 1;
+		if (t > 1) t -= 1;
+		if (t < 1/6) return p + (q - p) * 6 * t;
+		if (t < 1/2) return q;
+		if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+		return p;
+	}
+
+	var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+	var p = 2 * l - q;
+
+	r = hue2rgb(p, q, h + 1/3);
+	g = hue2rgb(p, q, h);
+	b = hue2rgb(p, q, h - 1/3);
+
+	r = Math.floor(r * 255);
+	g = Math.floor(g * 255);
+	b = Math.floor(b * 255);
+
+	//Convert to RGB color code
+	function componentToHex(c) {
+		var hex = c.toString(16);
+		return hex.length == 1 ? "0" + hex : hex;
+	}
+	function rgbToHex(r, g, b) {
+		return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+	}
+
+	return rgbToHex(r, g, b);
 }
 
 ///////////////////////////////////////////
